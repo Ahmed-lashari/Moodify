0bf3f3bea967864c1a2fb5b68e9f01a617ac10ae
Ahmed-lashari <ahmedlashari.official@gmail.com>
2025-12-23 20:12:44 +0500

    api key updated

--- frontend/src/api/post.js
+++ frontend/src/api/post.js
@@ -1,5 +1,5 @@
 export const sendLyrics = async (lyrics) => {
-  const res = await fetch(`${process.env.BACKEND_URL}/predict`, {
+  const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/predict`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ lyrics }),
